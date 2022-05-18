831bbc3385490a1c285a80b3cce2692d7cbaa847
ArcticRug25 <wyw82839148@163.com>
2022-05-18 23:48:59 +0800

    二叉树的最大深度

---
+++ 104.二叉树的最大深度.js
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode.cn id=104 lang=javascript
+ *
+ * [104] 二叉树的最大深度
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var maxDepth = function(root) {
+  if (root == null) return 0;
+  // 树最大深度，等于  左子树和右子树的深度对比，最大的一个加一
+  return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+};
+// @lc code=end
+
