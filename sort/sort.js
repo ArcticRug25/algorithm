37ff6289d5f8aef07291ee7d51339cdf3004e3f8
ArcticRug25 <wyw82839148@163.com>
2022-05-25 21:42:26 +0800

    快速排序

--- sort/sort.js
+++ sort/sort.js
@@ -12,4 +12,56 @@ function bubbleSort(arr) {
   return arr;
 }
 
+function quickSort(arr) {
+  if (arr.length < 2) return arr;
+
+  let flag = arr[0],
+    left = [],
+    right = [];
+
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] > flag) {
+      right.push(arr[i]);
+    } else {
+      left.push(arr[i]);
+    }
+  }
+
+  return quickSort(left).concat(flag, quickSort(right));
+}
+
+// 原地快排 （双指针） 左指针找到一个比标志位大的，右指针找到一个比标志位小的，则左指针的值和右指针值交换位置
+function quick(arr, start, end) {
+  let init = start;
+  let flag = arr[init];
+  start++;
+  while (start <= end) {
+    while (arr[end] > flag) {
+      end--;
+    }
+    while (arr[start] < flag) {
+      start++;
+    }
+    if (start < end) {
+      [arr[start], arr[end]] = [arr[end], arr[start]];
+      start++;
+      end--;
+    }
+  }
+  [arr[init], arr[start - 1]] = [arr[start - 1], arr[init]];
+  return start
+}
+
+function quickSort1(arr, start, end) {
+  if (start < end) {
+    let index = quick(arr, start, end); // 标志位的值
+    quickSort1(arr, start, index - 1);
+    quickSort1(arr, index, end);
+  }
+  return arr;
+}
+
+
 console.log(bubbleSort(arr));
+console.log(quickSort(arr));
+console.log(quickSort1(arr, 0, arr.length - 1));
